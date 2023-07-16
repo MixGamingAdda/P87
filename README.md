145e9d568b676151fa561281fd15bc1de545cb68
MixGamingAdda <86667122+MixGamingAdda@users.noreply.github.com>
2023-07-16 16:52:10 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# P87
+# spectagram-stage-7
+project solution for c87
---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAeyqDRYdVOShuGswsXa4pNGS_xDyZML7k",
+  authDomain: "spectogram-ced87.firebaseapp.com",
+  projectId: "spectogram-ced87",
+  storageBucket: "spectogram-ced87.appspot.com",
+  messagingSenderId: "110047413681",
+  appId: "1:110047413681:web:71ce987b1fa703b1cf2990"
+  };
